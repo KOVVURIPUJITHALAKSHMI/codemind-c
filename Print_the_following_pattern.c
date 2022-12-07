7bb7a421a5fde87d60e51dcd5c81e8d98867ee94
KOVVURIPUJITHALAKSHMI <119839999+KOVVURIPUJITHALAKSHMI@users.noreply.github.com>
2022-12-07 11:27:07 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for (int i=n;i>=1;i--)     {         for (int j=n-i+1;j<=n;j++)         {             printf("%d ",j);         }         printf(" ");     } }

---
+++ Print_the_following_pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for (int i=n;i>=1;i--)
+    {
+        for (int j=n-i+1;j<=n;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("
+");
+    }
+}
