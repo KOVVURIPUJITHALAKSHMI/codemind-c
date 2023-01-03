5808782f35c61073f3865017903d6f8c0c448a4b
KOVVURIPUJITHALAKSHMI <119839999+KOVVURIPUJITHALAKSHMI@users.noreply.github.com>
2023-01-03 10:32:22 +0530

    #include<stdio.h> int main() {     int nn,i,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&nn);         int c;         c=(nn+1)/2;         printf("%d ",c);     } }

---
+++ Playing_a_game_with_Hoops.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int nn,i,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&nn);
+        int c;
+        c=(nn+1)/2;
+        printf("%d
+",c);
+    }
+}
