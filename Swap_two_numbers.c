f184d7f8f8905e3ae0bf80cd5800c393cb529ff9
KOVVURIPUJITHALAKSHMI <119839999+KOVVURIPUJITHALAKSHMI@users.noreply.github.com>
2023-01-18 12:28:14 +0530

    #include<stdio.h> void swp(int a,int b) {     int t=a;     a=b;     b=t;     printf("%d %d",a,b); } int main() {     int a,b;     scanf("%d%d",&a,&b);     swp(a,b); }

--- Swap_two_numbers.c
+++ Swap_two_numbers.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
+void swp(int a,int b)
+{
+    int t=a;
+    a=b;
+    b=t;
+    printf("%d
+%d",a,b);
+}
 int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    printf("%d
-",b);
-    printf("%d",a);
+    swp(a,b);
 }
