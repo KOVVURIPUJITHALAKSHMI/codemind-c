0914f5eca500c5d406b7a1c75e114379d8350faf
KOVVURIPUJITHALAKSHMI <119839999+KOVVURIPUJITHALAKSHMI@users.noreply.github.com>
2023-05-08 12:19:14 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,x=0,y=1,c,z,w,m;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         if(a>n)         {             w=c-a;             break;         }         c=a+b;         a=b;         b=c;     }         for(int j=1;j<=n;j++)         {             if(x>n)             {                 m=x;                 break;             }             //printf("%d",x);             z=x+y;             x=y;             y=z;         }         int d1=n-w;         int d2=m-n;         if(d1<d2)         {             printf("%d",w);         }         else if(d2<d1)         {             printf("%d",m);
            }
            else if(d1==d2)
            {
                printf("%d %d",w,m);
            }
    }

--- nearest_fibonacci.c
+++ nearest_fibonacci.c
@@ -1,30 +1,44 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-    int i,n;
+    int n,a=0,b=1,x=0,y=1,c,z,w,m;
     scanf("%d",&n);
-    int a=0,b=1,c=0;
-    for(i=0;i<n;i++)
+    for(int i=1;i<=n;i++)
     {
-     if(c>=n)
-     {
-        break;
-     }
-       c=a+b;
-       a=b;
-       b=c;
+        if(a>n)
+        {
+            w=c-a;
+            break;
+        }
+        c=a+b;
+        a=b;
+        b=c;
     }
-    if(abs(n-a)==abs(n-c))
-    {
-        printf("%d %d",a,c);
-    }
-    else if(abs(n-a)>abs(n-c))
-    {
-        printf("%d",c);
-    }
-    else
-    {
-        printf("%d",a);
-    }
+        for(int j=1;j<=n;j++)
+        {
+            if(x>n)
+            {
+                m=x;
+                break;
+            }
+            //printf("%d",x);
+            z=x+y;
+            x=y;
+            y=z;
+        }
+        int d1=n-w;
+        int d2=m-n;
+        if(d1<d2)
+        {
+            printf("%d",w);
+        }
+        else if(d2<d1)
+        {
+            printf("%d",m);
+            
+        }
+        else if(d1==d2)
+        {
+            printf("%d %d",w,m);
+        }
 }
